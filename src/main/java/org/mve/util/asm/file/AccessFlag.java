6dda8d9509897e3a3512ed1987a2b94579152353
M8 <3390038158@qq.com>
2020-03-30 00:00:46 +0800

    Supplement all access flag

--- src/main/java/org/mve/util/asm/file/AccessFlag.java
+++ src/main/java/org/mve/util/asm/file/AccessFlag.java
@@ -8,7 +8,10 @@ public class AccessFlag
 	public static final short ACC_STATIC 		= 0x0008;
 	public static final short ACC_FINAL 		= 0x0010;
 	public static final short ACC_SUPER 		= 0x0020;
-	public static final short ACC_VOLATILE 		= 0x0040;
+	public static final short ACC_SYNCHRONIZED	= 0x0020;
+	public static final short ACC_VOLATILE		= 0x0040;
+	public static final short ACC_BRIDGE		= 0x0040;
+	public static final short ACC_VARARGS		= 0x0080;
 	public static final short ACC_TRANSIENT 	= 0x0080;
 	public static final short ACC_NATIVE 		= 0x0100;
 	public static final short ACC_INTERFACE 	= 0x0200;
