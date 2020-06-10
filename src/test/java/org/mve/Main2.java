f964a2685007372c97f927c5d05048d4170be1f4
M8 <3390038158@qq.com>
2020-06-10 21:45:40 +0800

    update

--- src/test/java/org/mve/Main2.java
+++ src/test/java/org/mve/Main2.java
@@ -5,8 +5,10 @@ import org.mve.invoke.ReflectionFactory;
 
 public class Main2
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws Throwable
 	{
+		Test test = new Test();
+
 		ReflectionAccessor<Test> accessor1 =
 			ReflectionFactory.getReflectionAccessor(
 				Test.class,
@@ -15,5 +17,32 @@ public class Main2
 				true,
 				true
 			);
+		accessor1.invoke(test);
+		System.out.println(accessor1.invoke());
+
+		ReflectionAccessor<String> accessor2 =
+			ReflectionFactory.getReflectionAccessor(
+				Test.class,
+				"staticPrefix",
+				String.class,
+				true,
+				false
+			);
+		accessor2.invoke("Test");
+		System.out.println(accessor2.invoke());
+
+		ReflectionAccessor<String> accessor3 =
+			ReflectionFactory.getReflectionAccessor(
+				Test.class.getDeclaredField("defaultPrefix")
+			);
+		accessor3.invoke(test, "TestDefaultPrefix");
+		System.out.println(accessor3.invoke(test));
+
+		ReflectionAccessor<String> accessor4 =
+			ReflectionFactory.getReflectionAccessor(
+				Test.class.getDeclaredField("prefix")
+			);
+		accessor4.invoke(test, "TestPrefix");
+		System.out.println(accessor4.invoke(test));
 	}
 }
