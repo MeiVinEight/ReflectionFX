90e2f07ef63eb809288d5644e9d437e6e92ef49a
M8 <3390038158@qq.com>
2020-06-10 22:34:18 +0800

    update

---
+++ src/test/java/org/mve/Main5.java
@@ -0,0 +1,23 @@
+package org.mve;
+
+import org.mve.invoke.EnumHelper;
+import org.mve.invoke.ReflectionFactory;
+
+import java.util.Arrays;
+
+public class Main5
+{
+	public static void main(String[] args)
+	{
+		EnumHelper<Test.TestEnum> helper = ReflectionFactory.getEnumHelper(Test.TestEnum.class);
+		Test.TestEnum value = helper.construct("D", 3);
+		System.out.println(Arrays.toString(helper.values()));
+		helper.add(value);
+		System.out.println(Arrays.toString(helper.values()));
+		helper.remove(2);
+		System.out.println(Arrays.toString(helper.values()));
+		Test.TestEnum a = helper.values()[0];
+		helper.values()[0] = null;
+		System.out.println(Arrays.toString(Test.TestEnum.values()));
+	}
+}
