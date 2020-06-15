99d48706d83f4f1b5109da91989aa22f2320595c
M8 <3390038158@qq.com>
2020-06-16 00:37:27 +0800

    New test

--- src/test/java/org/mve/Main.java
+++ src/test/java/org/mve/Main.java
@@ -1,31 +1,24 @@
 package org.mve;
 
-import org.mve.invoke.MethodKind;
+import org.mve.invoke.ReflectionAccessor;
 import org.mve.invoke.ReflectionFactory;
 import org.mve.util.SystemUtil;
+
+import java.util.Arrays;
 
 public class Main
 {
 	public static void main(String[] args)
 	{
-		BindSite site = new ReflectionFactory(BindSite.class, Main.class)
-			.method(
-				new MethodKind("a", void.class, String.class),
-				new MethodKind("a", void.class, String.class),
-				ReflectionFactory.KIND_INVOKE_STATIC
-			)
-			.allocate();
-		site.a("A");
+		ReflectionAccessor<Void> a = ReflectionFactory.getReflectionAccessor(Main.class, "a", true, false, false, void.class);
+		a.invoke();
+		a = ReflectionFactory.getReflectionAccessor(Main.class, "a", true, false, false, void.class);
+		a.invoke();
 	}
 
-	public interface BindSite
+	private static void a()
 	{
-		void a(String s);
-	}
-
-	private static void a(String s)
-	{
-		System.out.println(s);
 		SystemUtil.printStackTrace();
+		System.out.println(Arrays.toString(ReflectionFactory.ACCESSOR.getClassContext()));
 	}
 }
