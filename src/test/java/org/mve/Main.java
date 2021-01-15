7b01e14c7ce946bb8138f90752f4cee2ff999476
M8 <3390038158@qq.com>
2021-01-15 20:52:00 +0800

    New test code

--- src/test/java/org/mve/Main.java
+++ src/test/java/org/mve/Main.java
@@ -1,23 +1,11 @@
 package org.mve;
 
-import org.mve.invoke.ConstructorAccessor;
-import org.mve.invoke.FieldAccessor;
 import org.mve.invoke.ReflectionFactory;
-
-import java.lang.invoke.MethodType;
 
 public class Main
 {
-	private static final Object O = null;
 	public static void main(String[] args)
 	{
-		ConstructorAccessor<Main> ctr = ReflectionFactory.access(Main.class, MethodType.methodType(void.class));
-		System.out.println(ctr);
-		System.out.println(ctr.getConstructor());
-	}
-
-	private static void a()
-	{
-		new Throwable().printStackTrace(System.out);
+		System.out.println(ReflectionFactory.ACCESSOR.getPID());
 	}
 }
