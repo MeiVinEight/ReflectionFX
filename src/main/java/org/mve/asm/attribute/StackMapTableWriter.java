4c13c030eb3580672651febd720e5210ba8ca27e
M8 <3390038158@qq.com>
2021-06-06 21:19:17 +0800

    ADD StackMapTableWriter

---
+++ src/main/java/org/mve/asm/attribute/StackMapTableWriter.java
@@ -0,0 +1,80 @@
+package org.mve.asm.attribute;
+
+import org.mve.asm.ConstantPoolFinder;
+import org.mve.asm.Marker;
+import org.mve.asm.file.Attribute;
+import org.mve.asm.file.AttributeStackMapTable;
+import org.mve.asm.file.AttributeType;
+import org.mve.asm.file.ConstantPool;
+import org.mve.asm.stack.AppendFrame;
+import org.mve.asm.stack.ChopFrame;
+import org.mve.asm.stack.FullFrame;
+import org.mve.asm.stack.SameFrame;
+import org.mve.asm.stack.SameFrameExtended;
+import org.mve.asm.stack.SameLocals1StackItemFrame;
+import org.mve.asm.stack.SameLocals1StackItemFrameExtended;
+import org.mve.asm.stack.StackMapFrame;
+import org.mve.asm.stack.verification.Verification;
+
+import java.util.Arrays;
+
+public class StackMapTableWriter implements AttributeWriter
+{
+	private StackMapFrame[] frames = new StackMapFrame[0];
+
+	public StackMapTableWriter frame(StackMapFrame frame)
+	{
+		int i = this.frames.length;
+		this.frames = Arrays.copyOf(this.frames, i + 1);
+		this.frames[i] = frame;
+		return this;
+	}
+
+	public StackMapTableWriter sameFrame(Marker marker)
+	{
+		return this.frame(new SameFrame(marker));
+	}
+
+	public StackMapTableWriter sameLocals1StackItemFrame(Marker marker, Verification verification)
+	{
+		return this.frame(new SameLocals1StackItemFrame(marker, verification));
+	}
+
+	public StackMapTableWriter sameLocals1StackItemFrameExtended(Marker marker, Verification verification)
+	{
+		return this.frame(new SameLocals1StackItemFrameExtended(marker, verification));
+	}
+
+	public StackMapTableWriter chopFrame(Marker marker, int k)
+	{
+		return this.frame(new ChopFrame(marker, k));
+	}
+
+	public StackMapTableWriter sameFrameExtended(Marker marker)
+	{
+		return this.frame(new SameFrameExtended(marker));
+	}
+
+	public StackMapTableWriter appendFrame(Marker marker, Verification... verifications)
+	{
+		return this.frame(new AppendFrame(marker, verifications));
+	}
+
+	public StackMapTableWriter fullFrame(Marker marker, Verification[] local, Verification[] stack)
+	{
+		return this.frame(new FullFrame(marker, local, stack));
+	}
+
+	@Override
+	public Attribute getAttribute(ConstantPool pool)
+	{
+		AttributeStackMapTable smt = new AttributeStackMapTable((short) ConstantPoolFinder.findUTF8(pool, AttributeType.STACK_MAP_TABLE.getName()));
+		int offset = 0;
+		for (StackMapFrame frame : this.frames)
+		{
+			smt.addStackMapFrame(frame.transform(offset, pool));
+			offset = (frame.mark() + 1);
+		}
+		return smt;
+	}
+}
